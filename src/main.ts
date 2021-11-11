92efe28b28b7f0be0beaed528ba9bc83739e8e3c
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-11-11 15:15:25 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('EasyMarket API')
+    .setDescription('The EasyMarket API documentation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3333);
+}
+bootstrap();
