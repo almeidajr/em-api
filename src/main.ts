39cff2f1f7720e5d17be784f9fa3a9767f38c3b0
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-11-15 11:24:43 -0300

    Bind listen to env PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { defaultPort } from './settings/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,6 @@ async function bootstrap() {
     customSiteTitle: 'EasyMarket API Docs',
   });
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT || defaultPort);
 }
 bootstrap();
